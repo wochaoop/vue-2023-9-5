e60499be8475a574fc45624f2ad1173bd3f5e301
xiwangly2 <1334850101@qq.com>
2023-09-12 20:39:05 +0800

    成功了，使用bootstrap

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <h1>About Page</h1>
+    <div class="card" style="width: 18rem;">
+      <img src="https://www.xiwangly.top/avatar/Xiwangly.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AboutPage', // 使用多词组合
+  name: 'AboutPage'
 };
 </script>
