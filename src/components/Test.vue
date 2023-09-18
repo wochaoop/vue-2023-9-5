ee0093e8bdbc498e592df8e2407b3e7c394f10ad
xiwangly2 <1334850101@qq.com>
2023-09-18 15:53:57 +0800

    11111

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,11 +1,12 @@
 <template>
     <h1>Test Page</h1>
-    <div class="container p-5 test-while test-center border" >
+    <!-- <div class="container p-5 test-while test-center border" >
         <p>container</p>
     </div>
     <div class="container-fluid p-5 bg-primary test-while test-center" >
         <p>container-fluid</p>
-    </div>
+    </div> -->
+    <div class="col-lg-6" style="background-color: aqua;">111</div>
   </template>
   
   <script>
