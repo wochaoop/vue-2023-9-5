8cbe4623930c6c62154d5470201d559640a6bf8b
xiwangly2 <1334850101@qq.com>
2023-09-18 16:05:11 +0800

    bootstrap 网格应用

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,22 @@
 <template>
     <h1>Test Page</h1>
-    <!-- <div class="container p-5 test-while test-center border" >
-        <p>container</p>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4 bg-primary">这是一个蓝色背景的列</div>
+        <div class="col-md-8 bg-success">这是一个绿色背景的列</div>
+      </div>
     </div>
-    <div class="container-fluid p-5 bg-primary test-while test-center" >
-        <p>container-fluid</p>
-    </div> -->
-    <div class="col-lg-6" style="background-color: aqua;">111</div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 bg-warning">在大屏幕上占据整个宽度，黄色背景</div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 bg-danger">第一列，红色背景</div>
+        <div class="col-md-6 offset-md-2 bg-info">第二列，偏移两列，蓝色背景</div>
+      </div>
+    </div>
   </template>
   
   <script>
